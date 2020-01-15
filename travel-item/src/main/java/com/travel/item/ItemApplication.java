5542698d34477805c08440af47841a4a1c3cb413
a792644318 <a792644318@163.com>
2020-01-15 14:01:34 +0800

    update

--- travel-item/src/main/java/com/travel/item/ItemApplication.java
+++ travel-item/src/main/java/com/travel/item/ItemApplication.java
@@ -9,5 +9,6 @@ import tk.mybatis.spring.annotation.MapperScan;
 public class ItemApplication {
     public static void main(String[] args) {
         SpringApplication.run(ItemApplication.class);
+        System.out.println("haha");
     }
 }
